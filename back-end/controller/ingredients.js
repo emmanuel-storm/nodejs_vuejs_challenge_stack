988872283ee4ce4f0a31ecf3a9d566c05767decd
Pellegrin69 <pellegrinpierre@hotmail.fr>
2023-07-13 10:11:53 +0200

    Add try/catch and comments in controllers

--- back-end/controller/ingredients.js
+++ back-end/controller/ingredients.js
@@ -1,9 +1,11 @@
 const ingredients = require("../data/ingredients.json");
 
+// Récupère tous les ingrédients
 exports.getAllIngredients = (req, res) => {
     res.status(200).json(ingredients);
 }
 
+// Récupère un ingrédient spécifique par son ID
 exports.getIngredient = (req, res) => {
     const id = req.params.id;
     const ingredient = ingredients.find((ingredient) => ingredient.id === id);
--- back-end/controller/recipes.js
+++ back-end/controller/recipes.js
@@ -4,97 +4,129 @@ const ingredients = require("../data/ingredients.json");
 const {v4: uuidv4} = require("uuid");
 const fs = require("fs");
 
+// Récupère toutes les recettes
 exports.getAllRecipes = (req, res) => {
     res.status(200).json(recipes);
 }
 
+// Récupère une recette spécifique par son ID
 exports.getRecipe = (req, res) => {
     const id = req.params.id;
     const recipe = recipes.find((recipe) => recipe.id === id);
-    let recipe_data = recipe
 
     if (recipe) {
-        for (i = 0; i < recipe.ingredients.length; i++) {
-            let id_ingredient = recipe.ingredients[i].id
-            recipe_data.ingredients[i].name = ingredients.find((ingredient) => ingredient.id === id_ingredient).name;
-        }
+        try {
+            let recipe_data = recipe;
+            for (let i = 0; i < recipe.ingredients.length; i++) {
+                let id_ingredient = recipe.ingredients[i].id;
+                let ingredient = ingredients.find((ingredient) => ingredient.id === id_ingredient);
+                if (ingredient) {
+                    recipe_data.ingredients[i].name = ingredient.name;
+                } else {
+                    res.status(500).json({error: "Ingredient not found"});
+                }
+            }
             res.status(200).json(recipe_data);
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
     } else {
         res.status(404).json({error: "Recipe not found"});
     }
 }
 
+// Effectue une analyse nutritionnelle d'une recette spécifique
 exports.getRecipeAnalyse = (req, res) => {
     const id = req.params.id;
     const recipe = recipes.find((recipe) => recipe.id === id);
 
     if (recipe) {
-        let result = 0;
+        try {
+            let result = 0;
 
-        for (i = 0; i < recipe.ingredients.length; i++) {
-            let id_ingredient = recipe.ingredients[i].id
-            let quantity = recipe.ingredients[i].quantity
-            let caloriesPer100Units = ingredients.find((ingredient) => ingredient.id === id_ingredient).caloriesPer100Units
-            result += (quantity * (caloriesPer100Units / 100))
+            for (let i = 0; i < recipe.ingredients.length; i++) {
+                let id_ingredient = recipe.ingredients[i].id
+                let quantity = recipe.ingredients[i].quantity
+                let caloriesPer100Units = ingredients.find((ingredient) => ingredient.id === id_ingredient).caloriesPer100Units
+                if (caloriesPer100Units) {
+                    result += (quantity * (caloriesPer100Units / 100));
+                } else {
+                    res.status(500).json({error: "Calories per 100 units not found for an ingredient"});
+                }
+            }
+
+            res.status(200).json(Math.round(result));
+        } catch (error) {
+            res.status(500).json({ error: error.message });
         }
-
-        res.status(200).json(Math.round(result));
     } else {
         res.status(404).json({error: "Recipe not found"});
     }
 }
 
+// Crée une nouvelle recette
 exports.createRecipe = (req, res) => {
-    const newRecipe = req.body;
-    newRecipe.id = uuidv4();
+    try {
+        const newRecipe = req.body;
+        newRecipe.id = uuidv4();
 
-    // Ajouter les nouvelles données à votre structure JSON
-    recipes.push(newRecipe);
+        // Ajouter les nouvelles données à votre structure JSON
+        recipes.push(newRecipe);
 
-    // Enregistrer les modifications dans le fichier JSON
-    const fs = require('fs');
-    fs.writeFileSync('./data/recipes.json', JSON.stringify(recipes));
+        // Enregistrer les modifications dans le fichier JSON
+        fs.writeFileSync('./data/recipes.json', JSON.stringify(recipes));
 
-    res.status(201).json(newRecipe);
+        res.status(201).json(newRecipe);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 }
 
+// Met à jour une recette existante
 exports.updateRecipe = (req, res) => {
     const id = req.params.id;
     const updatedRecipe = req.body;
     const recipeIndex = recipes.findIndex((recipe) => recipe.id === id);
 
-
-    console.log(recipeIndex)
     if (recipeIndex !== -1) {
-        recipes[recipeIndex] = {...recipes[recipeIndex], ...updatedRecipe};
+        try {
+            recipes[recipeIndex] = {...recipes[recipeIndex], ...updatedRecipe};
 
-        // Écriture des modifications dans le fichier recipes.json
-        fs.writeFile('./data/recipes.json', JSON.stringify(recipes), (err) => {
-            if (err) {
-                res.status(500).json({error: 'Error writing to file'});
-            } else {
-                res.status(200).json(recipes[recipeIndex]);
-            }
-        });
+            // Écriture des modifications dans le fichier recipes.json
+            fs.writeFile('./data/recipes.json', JSON.stringify(recipes), (err) => {
+                if (err) {
+                    throw new Error("Error writing to file");
+                } else {
+                    res.status(200).json(recipes[recipeIndex]);
+                }
+            });
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
     } else {
         res.status(404).json({error: 'Recipe not found'});
     }
 }
 
+// Supprime une recette existante
 exports.deleteRecipe = (req, res) => {
     const id = req.params.id;
     const recipeIndex = recipes.findIndex((recipe) => recipe.id === id);
 
     if (recipeIndex !== -1) {
-        recipes.splice(recipeIndex, 1);
+        try {
+            recipes.splice(recipeIndex, 1);
 
-        fs.writeFile('./data/recipes.json', JSON.stringify(recipes), (err) => {
-            if (err) {
-                res.status(500).json({error: 'Error writing to file'});
-            } else {
-                res.status(200).json({ success: true });
-            }
-        });
+            fs.writeFile('./data/recipes.json', JSON.stringify(recipes), (err) => {
+                if (err) {
+                    throw new Error(`Error writing to file : ${err.message}`);
+                } else {
+                    res.status(200).json({ success: true });
+                }
+            });
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
     } else {
         res.status(404).json({error: 'Recipe not found'});
     }
